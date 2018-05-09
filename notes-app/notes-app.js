26f6c80df18f791a359a2c3f16f852f291a62f82
Brian Bycynski <high5bri@hotmail.com>
2018-05-09 09:43:18 -0400

    finished lecture 70

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,43 +1,26 @@
-const notes = [{
-    title: 'my next trip',
-    body: 'I would like to go to Spain'
-}, {
-    title: 'Habbits to work on',
-    body: 'Exercise. Eating a bit better.'
-}, {
-    title: 'Office modification',
-    body: 'Get a new seat'
-}]
+const notes = getSavedNotes();
 
 const filters = {
     searchText: ''
-}
+};
 
-const renderNotes = function (notes, filters) {
-    const filteredNotes = notes.filter(function (note) {
-        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-    })
+renderNotes(notes, filters);
 
-    document.querySelector('#notes').innerHTML = ''
-    
-    filteredNotes.forEach(function (note) {
-        const noteEl = document.createElement('p')
-        noteEl.textContent = note.title
-        document.querySelector('#notes').appendChild(noteEl)
-    })
-}
+document.querySelector('#create-note').addEventListener('click', function(e) {
+    notes.push({
+        id: uuidv4(),
+        title: '',
+        body: ''
+    });
+    saveNotes(notes);
+    renderNotes(notes, filters);
+});
 
-renderNotes(notes, filters)
+document.querySelector('#search-text').addEventListener('input', function(e) {
+    filters.searchText = e.target.value;
+    renderNotes(notes, filters);
+});
 
-document.querySelector('#create-note').addEventListener('click', function (e) {
-    e.target.textContent = 'The button was clicked'
-})
-
-document.querySelector('#search-text').addEventListener('input', function (e) {
-    filters.searchText = e.target.value
-    renderNotes(notes, filters)
-})
-
-document.querySelector('#filter-by').addEventListener('change', function (e) {
-    console.log(e.target.value)
-})
+document.querySelector('#filter-by').addEventListener('change', function(e) {
+    console.log(e.target.value);
+});
