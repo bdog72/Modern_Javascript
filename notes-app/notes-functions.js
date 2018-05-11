e9a5fc58ab2564c2e6e523db16da5b1942103711
Brian Bycynski <high5bri@hotmail.com>
2018-05-11 10:13:40 -0400

    finished up dates/moment()

--- notes-app/notes-functions.js
+++ notes-app/notes-functions.js
@@ -14,11 +14,12 @@ const saveNotes = function(notes) {
     localStorage.setItem('notes', JSON.stringify(notes));
 };
 
-// Remove note from list
+// Remove a note from the list
 const removeNote = function(id) {
     const noteIndex = notes.findIndex(function(note) {
         return note.id === id;
     });
+
     if (noteIndex > -1) {
         notes.splice(noteIndex, 1);
     }
@@ -27,7 +28,7 @@ const removeNote = function(id) {
 // Generate the DOM structure for a note
 const generateNoteDOM = function(note) {
     const noteEl = document.createElement('div');
-    const textEl = document.createElement('span');
+    const textEl = document.createElement('a');
     const button = document.createElement('button');
 
     // Setup the remove note button
@@ -45,13 +46,52 @@ const generateNoteDOM = function(note) {
     } else {
         textEl.textContent = 'Unnamed note';
     }
+    textEl.setAttribute('href', `/edit.html#${note.id}`);
     noteEl.appendChild(textEl);
 
     return noteEl;
 };
 
+// Sort your notes by 1 of 3 ways
+const sortNotes = function(notes, sortBy) {
+    if (sortBy === 'byEdited') {
+        return notes.sort(function(a, b) {
+            if (a.updatedAt > b.updatedAt) {
+                return -1;
+            } else if (a.updatedAt < b.updatedAt) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+    } else if (sortBy === 'byCreated') {
+        return notes.sort(function(a, b) {
+            if (a.createdAt > b.createdAt) {
+                return -1;
+            } else if (a.createdAt < b.createdAt) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+    } else if (sortBy === 'alphabetical') {
+        return notes.sort(function(a, b) {
+            if (a.title.toLowerCase() < b.title.toLowerCase()) {
+                return -1;
+            } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+    } else {
+        return notes;
+    }
+};
+
 // Render application notes
 const renderNotes = function(notes, filters) {
+    notes = sortNotes(notes, filters.sortBy);
     const filteredNotes = notes.filter(function(note) {
         return note.title
             .toLowerCase()
@@ -64,4 +104,9 @@ const renderNotes = function(notes, filters) {
         const noteEl = generateNoteDOM(note);
         document.querySelector('#notes').appendChild(noteEl);
     });
+};
+
+// Generate the last edited message
+const generateLastEdited = function(timestamp) {
+    return `Last edited ${moment(timestamp).fromNow()}`;
 };
