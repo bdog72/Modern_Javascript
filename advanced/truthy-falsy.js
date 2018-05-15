fddd5241353cf9c2caed59e577c4bb984f3eaf32
Brian Bycynski <high5bri@hotmail.com>
2018-05-15 16:44:58 -0400

    finished lecture 113

--- advanced/truthy-falsy.js
+++
@@ -1,8 +0,0 @@
-const products = [];
-const product = products[0];
-
-if (product) {
-    console.log('Product found');
-} else {
-    console.log('Product not found');
-}
--- advanced/type-coercion.js
+++
@@ -1,4 +0,0 @@
-const value = true + 12;
-const type = typeof value;
-console.log(type);
-console.log(value);
